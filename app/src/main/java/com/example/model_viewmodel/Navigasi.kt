709741247cda2e1dd9057f949ab734cef9e972e4
mufid1012 <mufidghibran1012@gmail.com>
2025-11-20 16:30:13 +0700

    Menambahkan composable SiswaApp sebagai root dengan ViewModel dan NavController

--- app/src/main/java/com/example/model_viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/model_viewmodel/Navigasi.kt
@@ -1,6 +1,21 @@
 package com.example.model_viewmodel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
 
 enum class Navigasi {
     Formulir,
     Detail
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+)
