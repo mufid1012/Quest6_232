a276f334742c0221215c9fab4ad26a9deebf830e
mufid1012 <mufidghibran1012@gmail.com>
2025-11-20 16:09:18 +0700

    menambahkan TopAppBar pada Scaffold

--- app/src/main/java/com/example/model_viewmodel/view/FormSiswa.kt
+++ app/src/main/java/com/example/model_viewmodel/view/FormSiswa.kt
@@ -1,9 +1,11 @@
 package com.example.model_viewmodel.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
 import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,6 +19,18 @@ fun FormSiswa(
     var txtAlamat by rememberSaveable { mutableStateOf(value = "") }
     var txtGender by rememberSaveable { mutableStateOf(value = "") }
     val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500)
+                )
+            )
+        }
+    )
 
 
 }
